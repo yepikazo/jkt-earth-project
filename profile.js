7b98c221f0a9e254d67875773a82177c83af47be
yepikazo <yasrafch@gmail.com>
2025-05-11 17:22:28 +0700

    Add files via upload

---
+++ profile.js
@@ -0,0 +1,30 @@
+const infoOji = document.getElementById("infoOji");
+const infoRifki = document.getElementById("infoRifki");
+const infoYep = document.getElementById("infoYep");
+const infoFadhel = document.getElementById("infoFadhel");
+const infoRakha = document.getElementById("infoRakha");
+
+infoOji.addEventListener("click", function () {
+    const infoOjiText = document.getElementById("infoOjiText");
+    alert("Oji");
+})
+
+infoRifki.addEventListener("click", function () {
+    const infoRifkiText = document.getElementById("infoRifkiText");
+    alert("Rifki");
+})
+
+infoYep.addEventListener("click", function () {
+    const infoYepText = document.getElementById("infoYepText");
+    alert("Yep");
+})
+
+infoFadhel.addEventListener("click", function () {
+    const infoFadhelText = document.getElementById("infoFadhelText");
+    alert("Fadhel");
+})
+
+infoRakha.addEventListener("click", function () {
+    const infoRakhaText = document.getElementById("infoRakhaText");
+    alert("Rakha");
+})
